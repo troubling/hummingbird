redbo/swift

go: container server and replicator
cbench on my SAIO with 10 concurrency, 5 containers, 50k objects:

SWIFT                               HUMMINGBIRD

OBJECT PUTs: 50000 @ 640.09/s       OBJECT PUTs: 50000 @ 2011.63/s
  Failures: 0                         Failures: 0
  Mean: 0.01562s (1403.0% RSD)        Mean: 0.00495s (867.7% RSD)
  Median: 0.00561s                    Median: 0.00038s
  85%: 0.00676s                       85%: 0.00045s
  90%: 0.00710s                       90%: 0.00047s
  95%: 0.00773s                       95%: 0.00067s
  99%: 0.01071s                       99%: 0.16002s
CONTAINER GETs: 500 @ 2.49/s        CONTAINER GETs: 500 @ 6.26/s
  Failures: 0                         Failures: 0
  Mean: 3.99264s (538.9% RSD)         Mean: 1.59099s (31.3% RSD)
  Median: 1.59760s                    Median: 1.49269s
  85%: 1.67616s                       85%: 2.11407s
  90%: 1.70633s                       90%: 2.26410s
  95%: 1.76960s                       95%: 2.60918s
  99%: 199.83902s                     99%: 3.09941s
OBJECT DELETEs: 50000 @ 632.68/s    OBJECT DELETEs: 50000 @ 1879.34/s
  Failures: 0                         Failures: 0
  Mean: 0.01580s (1440.8% RSD)        Mean: 0.00530s (815.9% RSD)
  Median: 0.00520s                    Median: 0.00037s
  85%: 0.00633s                       85%: 0.00045s
  90%: 0.00672s                       90%: 0.00048s
  95%: 0.00734s                       95%: 0.00467s
  99%: 0.00960s                       99%: 0.15108s

Median and mean object PUT/DELETE performance is way better.

The median GET times are only slightly better.  And the distribution
is kind of weird, with a lot more variation in hummingbird request
times.  I am going to dig into this more later.

Swift's GET mean is dragged down by a few crazy outlier requests that
don't complete until the rest of the benchmark finishes.  There may be
a locking bug in swift there.

There's one functional test that fails because it violates the http
spec somewhere that go

go/bench/cbench.go
@@ -1,0 +1,255 @@
+//  Copyright (c) 2015 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package bench
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/openstack/swift/go/hummingbird"
+)
+
+type ContainerObject struct {
+	Url string
+}
+
+func (obj *ContainerObject) Put() bool {
+	req, err := http.NewRequest("PUT", obj.Url, nil)
+	if err != nil {
+		return false
+	}
+	req.Header.Set("X-Timestamp", hummingbird.GetTimestamp())
+	req.Header.Set("X-Content-Type", "application/octet-stream")
+	req.Header.Set("X-Size", "0")
+	req.Header.Set("X-Etag", "d41d8cd98f00b204e9800998ecf8427e")
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	return err == nil && resp.StatusCode/100 == 2
+}
+
+func (obj *ContainerObject) Delete() bool {
+	req, _ := http.NewRequest("DELETE", obj.Url, nil)
+	req.Header.Set("X-Timestamp", hummingbird.GetTimestamp())
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	return err == nil && resp.StatusCode/100 == 2
+}
+
+func RunCBench(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: [configuration file]")
+		fmt.Println("The configuration file should look something like:")
+		fmt.Println("    [cbench]")
+		fmt.Println("    address = http://127.0.0.1:6011/")
+		fmt.Println("    containers = 5")
+		fmt.Println("    concurrency = 10")
+		fmt.Println("    num_objects = 10000")
+		fmt.Println("    num_gets = 500")
+		fmt.Println("    delete = yes")
+		fmt.Println("    check_mounted = false")
+		fmt.Println("    #drive_list = sdb1,sdb2")
+		os.Exit(1)
+	}
+
+	benchconf, err := hummingbird.LoadConfig(args[0])
+	if err != nil {
+		fmt.Println("Error parsing ini file:", err)
+		os.Exit(1)
+	}
+
+	address := benchconf.GetDefault("cbench", "address", "http://127.0.0.1:6011/")
+	if !strings.HasSuffix(address, "/") {
+		address = address + "/"
+	}
+	concurrency := int(benchconf.GetInt("cbench", "concurrency", 10))
+	containerCount := int(benchconf.GetInt("cbench", "containers", 5))
+	numObjects := benchconf.GetInt("cbench", "num_objects", 10000)
+	numGets := int(benchconf.GetInt("cbench", "num_gets", 500))
+	checkMounted := benchconf.GetBool("cbench", "check_mounted", false)
+	driveList := benchconf.GetDefault("cbench", "drive_list", "")
+	numPartitions := int64(100)
+	delete := benchconf.GetBool("cbench", "delete", true)
+
+	deviceList := GetDevices(address, checkMounted)
+	if driveList != "" {
+		deviceList = strings.Split(driveList, ",")
+	}
+
+	containers := make([]string, containerCount)
+	for i := 0; i < containerCount; i++ {
+		device := strings.Trim(deviceList[i%len(deviceList)], " ")
+		part := rand.Int63() % numPartitions
+		cid := rand.Int63()
+		containers[i] = fmt.Sprintf("%s%s/%d/%s/%d", address, device, part, "a", cid)
+		req, _ := http.NewRequest("PUT", containers[i], nil)
+		req.Header.Set("X-Timestamp", hummingbird.GetTimestamp())
+		resp, err := http.DefaultClient.Do(req)
+		if resp != nil {
+			resp.Body.Close()
+		}
+		if err != nil || resp.StatusCode/100 != 2 {
+			fmt.Println("Container PUT failed: ", resp.StatusCode, err)
+			os.Exit(1)
+		}
+	}
+
+	objects := make([]*ContainerObject, numObjects)
+	for i, _ := range objects {
+		container := containers[i%len(containers)]
+		objects[i] = &ContainerObject{
+			Url: fmt.Sprintf("%s/%d", container, rand.Int63()),
+		}
+	}
+
+	work := make([]func() bool, len(objects))
+	for i, _ := range objects {
+		work[i] = objects[i].Put
+	}
+	DoJobs("OBJECT PUT", work, concurrency)
+
+	time.Sleep(time.Second * 2)
+
+	getContainer := func() bool {
+		container := containers[rand.Int()%len(containers)]
+		req, _ := http.NewRequest("GET", container+"?format=json", nil)
+		resp, err := http.DefaultClient.Do(req)
+		if err == nil {
+			defer resp.Body.Close()
+			w, err := io.Copy(ioutil.Discard, resp.Body)
+			if err != nil || w < 2 {
+				return false
+			}
+		}
+		return err == nil && resp.StatusCode/100 == 2 && resp.Header.Get("Content-Type") == "application/json; charset=utf-8"
+	}
+	work = make([]func() bool, numGets)
+	for i := 0; i < numGets; i++ {
+		work[i] = getContainer
+	}
+	DoJobs("CONTAINER GET", work, concurrency)
+
+	time.Sleep(time.Second * 2)
+
+	if delete {
+		work = make([]func() bool, len(objects))
+		for i, _ := range objects {
+			work[i] = objects[i].Delete
+		}
+		DoJobs("OBJECT DELETE", work, concurrency)
+	}
+}
+
+func RunCGBench(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: [configuration file]")
+		fmt.Println("The configuration file should look something like:")
+		fmt.Println("    [cgbench]")
+		fmt.Println("    address = http://127.0.0.1:6011/")
+		fmt.Println("    concurrency = 5")
+		fmt.Println("    num_objects = 5000000")
+		fmt.Println("    report_interval = 100000")
+		fmt.Println("    num_gets = 10")
+		fmt.Println("    check_mounted = false")
+		fmt.Println("    #drive_list = sdb1,sdb2")
+		os.Exit(1)
+	}
+
+	benchconf, err := hummingbird.LoadConfig(args[0])
+	if err != nil {
+		fmt.Println("Error parsing ini file:", err)
+		os.Exit(1)
+	}
+
+	address := benchconf.GetDefault("cgbench", "address", "http://127.0.0.1:6011/")
+	if !strings.HasSuffix(address, "/") {
+		address = address + "/"
+	}
+	concurrency := int(benchconf.GetInt("cgbench", "concurrency", 5))
+	reportInterval := benchconf.GetInt("cgbench", "report_interval", 100000)
+	numObjects := benchconf.GetInt("cgbench", "num_objects", 5000000)
+	numGets := int(benchconf.GetInt("cgbench", "num_gets", 10))
+	checkMounted := benchconf.GetBool("cgbench", "check_mounted", false)
+	driveList := benchconf.GetDefault("cgbench", "drive_list", "")
+	numPartitions := int64(100)
+
+	deviceList := GetDevices(address, checkMounted)
+	if driveList != "" {
+		deviceList = strings.Split(driveList, ",")
+	}
+
+	device := strings.Trim(deviceList[0], " ")
+	part := rand.Int63() % numPartitions
+	cid := rand.Int63()
+	container := fmt.Sprintf("%s%s/%d/%s/%d", address, device, part, "a", cid)
+	req, _ := http.NewRequest("PUT", container, nil)
+	req.Header.Set("X-Timestamp", hummingbird.GetTimestamp())
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	if err != nil || resp.StatusCode/100 != 2 {
+		fmt.Println("Container PUT failed: ", resp.StatusCode, err)
+		os.Exit(1)
+	}
+
+	for totalObjects := int64(0); totalObjects < numObjects; totalObjects += reportInterval {
+		fmt.Println("Object count:", totalObjects)
+		objects := make([]*ContainerObject, int(reportInterval))
+		for i, _ := range objects {
+			objects[i] = &ContainerObject{
+				Url: fmt.Sprintf("%s/%d", container, rand.Int63()),
+			}
+		}
+
+		work := make([]func() bool, len(objects))
+		for i, _ := range objects {
+			work[i] = objects[i].Put
+		}
+		DoJobs("OBJECT PUT", work, concurrency)
+
+		time.Sleep(time.Second * 2)
+
+		getContainer := func() bool {
+			req, _ := http.NewRequest("GET", container+"?format=json&marker=5", nil)
+			resp, err := http.DefaultClient.Do(req)
+			if err == nil {
+				defer resp.Body.Close()
+				w, err := io.Copy(ioutil.Discard, resp.Body)
+				if err != nil || w < 2 {
+					return false
+				}
+			}
+			return err == nil && resp.StatusCode/100 == 2 && resp.Header.Get("Content-Type") == "application/json; charset=utf-8"
+		}
+		work = make([]func() bool, numGets)
+		for i := 0; i < numGets; i++ {
+			work[i] = getContainer
+		}
+		DoJobs("CONTAINER GET", work, concurrency)
+
+		time.Sleep(time.Second * 2)
+	}
+}

go/hummingbird/syncrealm.go
@@ -1,0 +1,75 @@
+//  Copyright (c) 2015 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package hummingbird
+
+import "strings"
+
+type SyncRealm struct {
+	Name     string
+	Key1     string
+	Key2     string
+	Clusters map[string]string
+}
+
+type SyncRealmList map[string]SyncRealm
+
+func (l SyncRealmList) ValidateSyncTo(syncHeader string) bool {
+	if !strings.HasPrefix(syncHeader, "//") {
+		return false
+	}
+	parts := strings.Split(syncHeader[2:], "/")
+	if len(parts) < 4 {
+		return false
+	}
+	realm := parts[0]
+	cluster := parts[1]
+	account := parts[2]
+	container := parts[3]
+	if account == "" || container == "" {
+		return false
+	}
+	if l[realm].Key1 == "" {
+		return false
+	}
+	if l[realm].Clusters[cluster] == "" {
+		return false
+	}
+	return true
+}
+
+var syncRealmConfigLocations = []string{"/etc/hummingbird/container-sync-realms.conf", "/etc/swift/container-sync-realms.conf"}
+
+func GetSyncRealms() SyncRealmList {
+	resp := make(map[string]SyncRealm)
+	for _, loc := range syncRealmConfigLocations {
+		if conf, err := LoadConfig(loc); err == nil {
+			for realm, config := range conf.File {
+				realm := SyncRealm{Name: realm, Clusters: make(map[string]string)}
+				for k, v := range config {
+					if k == "key" {
+						realm.Key1 = v
+					} else if k == "key2" {
+						realm.Key2 = v
+					} else if strings.HasPrefix(k, "cluster_") {
+						realm.Clusters[k[8:]] = v
+					}
+				}
+				resp[realm.Name] = realm
+			}
+		}
+	}
+	return SyncRealmList(resp)
+}

go/containerserver/update.go
@@ -1,0 +1,75 @@
+//  Copyright (c) 2016 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package containerserver
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/openstack/swift/go/hummingbird"
+)
+
+var waitForAccountUpdate = time.Second * 5
+
+func (server *ContainerServer) accountUpdate(request *http.Request, vars map[string]string, info *ContainerInfo, logger hummingbird.LoggingContext) {
+	firstDone := make(chan struct{}, 1)
+	go func() {
+		defer func() { firstDone <- struct{}{} }()
+		defer logger.LogPanics("PANIC WHILE UPDATING ACCOUNT")
+		accpartition := request.Header.Get("X-Account-Partition")
+		if accpartition == "" {
+			logger.LogError("Account update failed: bad partition")
+			return
+		}
+		hosts := strings.Split(request.Header.Get("X-Account-Host"), ",")
+		devices := strings.Split(request.Header.Get("X-Account-Device"), ",")
+		if len(hosts) != len(devices) {
+			logger.LogError("Account update failed: different numbers of hosts and devices in request")
+			return
+		}
+		for index, host := range hosts {
+			url := fmt.Sprintf("http://%s/%s/%s/%s/%s", host, devices[index], accpartition,
+				hummingbird.Urlencode(vars["account"]), hummingbird.Urlencode(vars["container"]))
+			req, err := http.NewRequest("PUT", url, nil)
+			if err != nil {
+				logger.LogError("Account update failed: error creating request object")
+				continue
+			}
+			req.Header.Add("X-Put-Timestamp", info.PutTimestamp)
+			req.Header.Add("X-Delete-Timestamp", info.DeleteTimestamp)
+			req.Header.Add("X-Object-Count", strconv.FormatInt(info.ObjectCount, 10))
+			req.Header.Add("X-Bytes-Used", strconv.FormatInt(info.BytesUsed, 10))
+			req.Header.Add("X-Trans-Id", request.Header.Get("X-Trans-Id"))
+			req.Header.Add("X-Backend-Storage-Policy-Index", strconv.Itoa(info.StoragePolicyIndex))
+			if request.Header.Get("X-Account-Override-Deleted") == "yes" {
+				req.Header.Add("X-Account-Override-Deleted", "yes")
+			}
+			resp, err := server.updateClient.Do(req)
+			defer resp.Body.Close()
+			if err != nil || (resp.StatusCode/100) != 2 {
+				logger.LogError("Account update failed: bad response from %s/%s", hosts[index], devices[index])
+				continue
+			}
+		}
+	}()
+	select {
+	case <-time.After(waitForAccountUpdate):
+	case <-firstDone:
+	}
+}

troubling/hummingbird

bench/cbench.go
@@ -1,0 +1,256 @@
+//  Copyright (c) 2015 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package bench
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/troubling/hummingbird/common"
+	"github.com/troubling/hummingbird/common/conf"
+)
+
+type ContainerObject struct {
+	Url string
+}
+
+func (obj *ContainerObject) Put() bool {
+	req, err := http.NewRequest("PUT", obj.Url, nil)
+	if err != nil {
+		return false
+	}
+	req.Header.Set("X-Timestamp", common.GetTimestamp())
+	req.Header.Set("X-Content-Type", "application/octet-stream")
+	req.Header.Set("X-Size", "0")
+	req.Header.Set("X-Etag", "d41d8cd98f00b204e9800998ecf8427e")
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	return err == nil && resp.StatusCode/100 == 2
+}
+
+func (obj *ContainerObject) Delete() bool {
+	req, _ := http.NewRequest("DELETE", obj.Url, nil)
+	req.Header.Set("X-Timestamp", common.GetTimestamp())
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	return err == nil && resp.StatusCode/100 == 2
+}
+
+func RunCBench(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: [configuration file]")
+		fmt.Println("The configuration file should look something like:")
+		fmt.Println("    [cbench]")
+		fmt.Println("    address = http://127.0.0.1:6011/")
+		fmt.Println("    containers = 5")
+		fmt.Println("    concurrency = 10")
+		fmt.Println("    num_objects = 10000")
+		fmt.Println("    num_gets = 500")
+		fmt.Println("    delete = yes")
+		fmt.Println("    check_mounted = false")
+		fmt.Println("    #drive_list = sdb1,sdb2")
+		os.Exit(1)
+	}
+
+	benchconf, err := conf.LoadConfig(args[0])
+	if err != nil {
+		fmt.Println("Error parsing ini file:", err)
+		os.Exit(1)
+	}
+
+	address := benchconf.GetDefault("cbench", "address", "http://127.0.0.1:6011/")
+	if !strings.HasSuffix(address, "/") {
+		address = address + "/"
+	}
+	concurrency := int(benchconf.GetInt("cbench", "concurrency", 10))
+	containerCount := int(benchconf.GetInt("cbench", "containers", 5))
+	numObjects := benchconf.GetInt("cbench", "num_objects", 10000)
+	numGets := int(benchconf.GetInt("cbench", "num_gets", 500))
+	checkMounted := benchconf.GetBool("cbench", "check_mounted", false)
+	driveList := benchconf.GetDefault("cbench", "drive_list", "")
+	numPartitions := int64(100)
+	delete := benchconf.GetBool("cbench", "delete", true)
+
+	deviceList := GetDevices(address, checkMounted)
+	if driveList != "" {
+		deviceList = strings.Split(driveList, ",")
+	}
+
+	containers := make([]string, containerCount)
+	for i := 0; i < containerCount; i++ {
+		device := strings.Trim(deviceList[i%len(deviceList)], " ")
+		part := rand.Int63() % numPartitions
+		cid := rand.Int63()
+		containers[i] = fmt.Sprintf("%s%s/%d/%s/%d", address, device, part, "a", cid)
+		req, _ := http.NewRequest("PUT", containers[i], nil)
+		req.Header.Set("X-Timestamp", common.GetTimestamp())
+		resp, err := http.DefaultClient.Do(req)
+		if resp != nil {
+			resp.Body.Close()
+		}
+		if err != nil || resp.StatusCode/100 != 2 {
+			fmt.Println("Container PUT failed: ", resp.StatusCode, err)
+			os.Exit(1)
+		}
+	}
+
+	objects := make([]*ContainerObject, numObjects)
+	for i, _ := range objects {
+		container := containers[i%len(containers)]
+		objects[i] = &ContainerObject{
+			Url: fmt.Sprintf("%s/%d", container, rand.Int63()),
+		}
+	}
+
+	work := make([]func() bool, len(objects))
+	for i, _ := range objects {
+		work[i] = objects[i].Put
+	}
+	DoJobs("OBJECT PUT", work, concurrency)
+
+	time.Sleep(time.Second * 2)
+
+	getContainer := func() bool {
+		container := containers[rand.Int()%len(containers)]
+		req, _ := http.NewRequest("GET", container+"?format=json", nil)
+		resp, err := http.DefaultClient.Do(req)
+		if err == nil {
+			defer resp.Body.Close()
+			w, err := io.Copy(ioutil.Discard, resp.Body)
+			if err != nil || w < 2 {
+				return false
+			}
+		}
+		return err == nil && resp.StatusCode/100 == 2 && resp.Header.Get("Content-Type") == "application/json; charset=utf-8"
+	}
+	work = make([]func() bool, numGets)
+	for i := 0; i < numGets; i++ {
+		work[i] = getContainer
+	}
+	DoJobs("CONTAINER GET", work, concurrency)
+
+	time.Sleep(time.Second * 2)
+
+	if delete {
+		work = make([]func() bool, len(objects))
+		for i, _ := range objects {
+			work[i] = objects[i].Delete
+		}
+		DoJobs("OBJECT DELETE", work, concurrency)
+	}
+}
+
+func RunCGBench(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: [configuration file]")
+		fmt.Println("The configuration file should look something like:")
+		fmt.Println("    [cgbench]")
+		fmt.Println("    address = http://127.0.0.1:6011/")
+		fmt.Println("    concurrency = 5")
+		fmt.Println("    num_objects = 5000000")
+		fmt.Println("    report_interval = 100000")
+		fmt.Println("    num_gets = 10")
+		fmt.Println("    check_mounted = false")
+		fmt.Println("    #drive_list = sdb1,sdb2")
+		os.Exit(1)
+	}
+
+	benchconf, err := conf.LoadConfig(args[0])
+	if err != nil {
+		fmt.Println("Error parsing ini file:", err)
+		os.Exit(1)
+	}
+
+	address := benchconf.GetDefault("cgbench", "address", "http://127.0.0.1:6011/")
+	if !strings.HasSuffix(address, "/") {
+		address = address + "/"
+	}
+	concurrency := int(benchconf.GetInt("cgbench", "concurrency", 5))
+	reportInterval := benchconf.GetInt("cgbench", "report_interval", 100000)
+	numObjects := benchconf.GetInt("cgbench", "num_objects", 5000000)
+	numGets := int(benchconf.GetInt("cgbench", "num_gets", 10))
+	checkMounted := benchconf.GetBool("cgbench", "check_mounted", false)
+	driveList := benchconf.GetDefault("cgbench", "drive_list", "")
+	numPartitions := int64(100)
+
+	deviceList := GetDevices(address, checkMounted)
+	if driveList != "" {
+		deviceList = strings.Split(driveList, ",")
+	}
+
+	device := strings.Trim(deviceList[0], " ")
+	part := rand.Int63() % numPartitions
+	cid := rand.Int63()
+	container := fmt.Sprintf("%s%s/%d/%s/%d", address, device, part, "a", cid)
+	req, _ := http.NewRequest("PUT", container, nil)
+	req.Header.Set("X-Timestamp", common.GetTimestamp())
+	resp, err := http.DefaultClient.Do(req)
+	if resp != nil {
+		resp.Body.Close()
+	}
+	if err != nil || resp.StatusCode/100 != 2 {
+		fmt.Println("Container PUT failed: ", resp.StatusCode, err)
+		os.Exit(1)
+	}
+
+	for totalObjects := int64(0); totalObjects < numObjects; totalObjects += reportInterval {
+		fmt.Println("Object count:", totalObjects)
+		objects := make([]*ContainerObject, int(reportInterval))
+		for i, _ := range objects {
+			objects[i] = &ContainerObject{
+				Url: fmt.Sprintf("%s/%d", container, rand.Int63()),
+			}
+		}
+
+		work := make([]func() bool, len(objects))
+		for i, _ := range objects {
+			work[i] = objects[i].Put
+		}
+		DoJobs("OBJECT PUT", work, concurrency)
+
+		time.Sleep(time.Second * 2)
+
+		getContainer := func() bool {
+			req, _ := http.NewRequest("GET", container+"?format=json&marker=5", nil)
+			resp, err := http.DefaultClient.Do(req)
+			if err == nil {
+				defer resp.Body.Close()
+				w, err := io.Copy(ioutil.Discard, resp.Body)
+				if err != nil || w < 2 {
+					return false
+				}
+			}
+			return err == nil && resp.StatusCode/100 == 2 && resp.Header.Get("Content-Type") == "application/json; charset=utf-8"
+		}
+		work = make([]func() bool, numGets)
+		for i := 0; i < numGets; i++ {
+			work[i] = getContainer
+		}
+		DoJobs("CONTAINER GET", work, concurrency)
+
+		time.Sleep(time.Second * 2)
+	}
+}

common/conf/syncrealm.go
@@ -1,0 +1,75 @@
+//  Copyright (c) 2015 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package conf
+
+import "strings"
+
+type SyncRealm struct {
+	Name     string
+	Key1     string
+	Key2     string
+	Clusters map[string]string
+}
+
+type SyncRealmList map[string]SyncRealm
+
+func (l SyncRealmList) ValidateSyncTo(syncHeader string) bool {
+	if !strings.HasPrefix(syncHeader, "//") {
+		return false
+	}
+	parts := strings.Split(syncHeader[2:], "/")
+	if len(parts) < 4 {
+		return false
+	}
+	realm := parts[0]
+	cluster := parts[1]
+	account := parts[2]
+	container := parts[3]
+	if account == "" || container == "" {
+		return false
+	}
+	if l[realm].Key1 == "" {
+		return false
+	}
+	if l[realm].Clusters[cluster] == "" {
+		return false
+	}
+	return true
+}
+
+var syncRealmConfigLocations = []string{"/etc/hummingbird/container-sync-realms.conf", "/etc/swift/container-sync-realms.conf"}
+
+func GetSyncRealms() SyncRealmList {
+	resp := make(map[string]SyncRealm)
+	for _, loc := range syncRealmConfigLocations {
+		if conf, err := LoadConfig(loc); err == nil {
+			for realm, config := range conf.File {
+				realm := SyncRealm{Name: realm, Clusters: make(map[string]string)}
+				for k, v := range config {
+					if k == "key" {
+						realm.Key1 = v
+					} else if k == "key2" {
+						realm.Key2 = v
+					} else if strings.HasPrefix(k, "cluster_") {
+						realm.Clusters[k[8:]] = v
+					}
+				}
+				resp[realm.Name] = realm
+			}
+		}
+	}
+	return SyncRealmList(resp)
+}

containerserver/update.go
@@ -1,0 +1,79 @@
+//  Copyright (c) 2016 Rackspace
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package containerserver
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/troubling/hummingbird/common"
+	"github.com/troubling/hummingbird/common/srv"
+)
+
+var waitForAccountUpdate = time.Second * 5
+
+func (server *ContainerServer) accountUpdate(request *http.Request, vars map[string]string, info *ContainerInfo, logger srv.LoggingContext) {
+	firstDone := make(chan struct{}, 1)
+	go func() {
+		defer func() { firstDone <- struct{}{} }()
+		defer logger.LogPanics("PANIC WHILE UPDATING ACCOUNT")
+		accpartition := request.Header.Get("X-Account-Partition")
+		if accpartition == "" {
+			logger.LogError("Account update failed: bad partition")
+			return
+		}
+		hosts := strings.Split(request.Header.Get("X-Account-Host"), ",")
+		devices := strings.Split(request.Header.Get("X-Account-Device"), ",")
+		if len(hosts) != len(devices) {
+			logger.LogError("Account update failed: different numbers of hosts and devices in request")
+			return
+		}
+		for index, host := range hosts {
+			url := fmt.Sprintf("http://%s/%s/%s/%s/%s", host, devices[index], accpartition,
+				common.Urlencode(vars["account"]), common.Urlencode(vars["container"]))
+			req, err := http.NewRequest("PUT", url, nil)
+			if err != nil {
+				logger.LogError("Account update failed: error creating request object")
+				continue
+			}
+			req.Header.Add("X-Put-Timestamp", info.PutTimestamp)
+			req.Header.Add("X-Delete-Timestamp", info.DeleteTimestamp)
+			req.Header.Add("X-Object-Count", strconv.FormatInt(info.ObjectCount, 10))
+			req.Header.Add("X-Bytes-Used", strconv.FormatInt(info.BytesUsed, 10))
+			req.Header.Add("X-Trans-Id", request.Header.Get("X-Trans-Id"))
+			req.Header.Add("X-Backend-Storage-Policy-Index", strconv.Itoa(info.StoragePolicyIndex))
+			if request.Header.Get("X-Account-Override-Deleted") == "yes" {
+				req.Header.Add("X-Account-Override-Deleted", "yes")
+			}
+			resp, err := server.updateClient.Do(req)
+			if err != nil {
+				logger.LogError("Account update failed: bad response from %s/%s", hosts[index], devices[index])
+				continue
+			}
+			defer resp.Body.Close()
+			if (resp.StatusCode / 100) != 2 {
+				logger.LogError("Account update failed: bad response from %s/%s", hosts[index], devices[index])
+			}
+		}
+	}()
+	select {
+	case <-time.After(waitForAccountUpdate):
+	case <-firstDone:
+	}
+}