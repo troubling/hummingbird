5aaf47cdefdce9a7aba2d408dae4da17dca74d1e
Greg Holt <gregory.holt@gmail.com>
2017-06-28 16:52:16 -0700

    Fixing things from functests test_container.py

--- containerserver/server.go
+++ containerserver/server.go
@@ -272,7 +272,11 @@ func (server *ContainerServer) ContainerPutHandler(writer http.ResponseWriter, r
 	}
 	created, db, err := server.containerEngine.Create(vars, timestamp, metadata, policyIndex, defaultPolicyIndex)
 	if err == ErrorPolicyConflict {
-		srv.StandardResponse(writer, http.StatusConflict)
+		if policyIndex == -1 {
+			srv.StandardResponse(writer, http.StatusAccepted)
+		} else {
+			srv.StandardResponse(writer, http.StatusConflict)
+		}
 		return
 	} else if err != nil {
 		srv.GetLogger(request).Error("Unable to create database.", zap.Error(err))
