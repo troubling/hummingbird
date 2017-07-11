8ec862716e0ac04c95abe3229dd05d79158d63ed
Greg Holt <gregory.holt@gmail.com>
2017-07-11 13:59:43 -0700

    Added "upload" to nectar

--- cmd/nectar/main.go
+++ cmd/nectar/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gholt/brimtext"
 	"github.com/troubling/hummingbird/client"
@@ -25,28 +26,33 @@ var (
 	globalFlagAuthKey         = globalFlags.String("K", os.Getenv("AUTH_KEY"), "|<key>| Key for auth system, example: testing - Some auth systems use passwords instead, see -P - Env: AUTH_KEY")
 	globalFlagAuthPassword    = globalFlags.String("P", os.Getenv("AUTH_PASSWORD"), "|<password>| Password for auth system, example: testing - Some auth system use keys instead, see -K - Env: AUTH_PASSWORD")
 	globalFlagStorageRegion   = globalFlags.String("R", os.Getenv("STORAGE_REGION"), "|<region>| Storage region to use if set, otherwise uses the default. Env: STORAGE_REGION")
+	globalFlagVerbose         = globalFlags.Bool("v", false, "Will activate verbose output.")
+	globalFlagContinueOnError = globalFlags.Bool("continue-on-error", false, "When possible, continue with additional operations even if one or more fail.")
+	globalFlagConcurrency     *int               // defined in init()
 	globalFlagInternalStorage *bool              // defined in init()
 	globalFlagHeaders         = stringListFlag{} // defined in init()
 )
 
 var (
-	getFlags         = flag.NewFlagSet("get", flag.ExitOnError)
+	getFlags         = flag.NewFlagSet("get", flag.ContinueOnError)
 	getFlagRaw       = getFlags.Bool("r", false, "Emit raw results")
 	getFlagNameOnly  = getFlags.Bool("n", false, "In listings, emits the names only")
 	getFlagMarker    = getFlags.String("marker", "", "|<text>| In listings, sets the start marker")
 	getFlagEndMarker = getFlags.String("endmarker", "", "|<text>| In listings, sets the stop marker")
 	getFlagReverse   = getFlags.Bool("reverse", false, "In listings, reverses the order")
 	getFlagLimit     = getFlags.Int("limit", 0, "|<number>| In listings, limits the results")
-	getFlagPrefix    = getFlags.String("prefix", "", "|<text>|In listings, returns only those matching the prefix")
+	getFlagPrefix    = getFlags.String("prefix", "", "|<text>| In listings, returns only those matching the prefix")
 	getFlagDelimiter = getFlags.String("delimiter", "", "|<text>| In listings, sets the delimiter and activates delimiter listings")
 )
 
 var (
-	headFlags   = flag.NewFlagSet("head", flag.ExitOnError)
+	headFlags   = flag.NewFlagSet("head", flag.ContinueOnError)
 	headFlagRaw = headFlags.Bool("r", false, "Emit raw headers")
 )
 
 func init() {
+	i32, _ := strconv.ParseInt(os.Getenv("CONCURRENCY"), 10, 32)
+	globalFlagConcurrency = globalFlags.Int("C", int(i32), "|<number>| The maximum number of concurrent operations to perform; default is 1. Env: CONCURRENCY")
 	b, _ := strconv.ParseBool(os.Getenv("STORAGE_INTERNAL"))
 	globalFlagInternalStorage = globalFlags.Bool("I", b, "Internal storage URL resolution, such as Rackspace ServiceNet. Env: STORAGE_INTERNAL")
 	globalFlags.Var(&globalFlagHeaders, "H", "|<name>:[value]| Sets a header to be sent with the request. Useful mostly for PUTs and POSTs, allowing you to set metadata. This option can be specified multiple times for additional headers.")
@@ -56,40 +62,43 @@ func init() {
 	headFlags.SetOutput(&flagbuf)
 }
 
-func help(flags *flag.FlagSet, err error) {
+func fatal(err error) {
 	if err == flag.ErrHelp || err == nil {
-		fmt.Fprintln(os.Stderr, os.Args[0], `[options] <subcommand> ...`)
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println(os.Args[0], `[options] <subcommand> ...`)
+		fmt.Println(brimtext.Wrap(`
 Tool for accessing a Hummingbird/Swift cluster. Some global options can also be set via environment variables. These will be noted at the end of the description with Env: NAME. The following global options are available:
         `, 0, "  ", "  "))
 		helpFlags(globalFlags)
-		fmt.Fprintln(os.Stderr)
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println()
+		fmt.Println(brimtext.Wrap(`
 The following subcommands are available:`, 0, "", ""))
-		fmt.Fprintln(os.Stderr)
-		fmt.Fprintln(os.Stderr, "\ndelete [container] [object]")
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println("\ndelete [container] [object]")
+		fmt.Println(brimtext.Wrap(`
 Performs a DELETE request. A DELETE, as probably expected, is used to remove the target.
         `, 0, "  ", "  "))
-		fmt.Fprintln(os.Stderr, "\nget [options] [container] [object]")
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println("\nget [options] [container] [object]")
+		fmt.Println(brimtext.Wrap(`
 Performs a GET request. A GET on an account or container will output the listing of containers or objects, respectively. A GET on an object will output the content of the object to standard output.
         `, 0, "  ", "  "))
 		helpFlags(getFlags)
-		fmt.Fprintln(os.Stderr, "\nhead [options] [container] [object]")
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println("\nhead [options] [container] [object]")
+		fmt.Println(brimtext.Wrap(`
 Performs a HEAD request, giving overall information about the account, container, or object.
         `, 0, "  ", "  "))
 		helpFlags(headFlags)
-		fmt.Fprintln(os.Stderr, "\npost [container] [object]")
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println("\npost [container] [object]")
+		fmt.Println(brimtext.Wrap(`
 Performs a POST request. POSTs allow you to update the metadata for the target.
         `, 0, "  ", "  "))
-		fmt.Fprintln(os.Stderr, "\nput [container] [object]")
-		fmt.Fprintln(os.Stderr, brimtext.Wrap(`
+		fmt.Println("\nput [container] [object]")
+		fmt.Println(brimtext.Wrap(`
 Performs a PUT request. A PUT to an account or container will create them. A PUT to an object will create it using the content from standard input.
         `, 0, "  ", "  "))
-		fmt.Fprintln(os.Stderr, "\n[container] [object] can also be specified as [container]/[object]")
+		fmt.Println("\nupload [options] <sourcepath> [container] [object]")
+		fmt.Println(brimtext.Wrap(`
+Uploads local files as objects. If you don't specify [container] the name of the current directory will be used. If you don't specify [object] the relative path name from the current directory will be used. If you do specify [object] while uploading a directory, [object] will be used as a prefix to the resulting object names. Note that when uploading a directory, only regular files will be uploaded.
+        `, 0, "  ", "  "))
+		fmt.Println("\n[container] [object] can also be specified as [container]/[object]")
 	} else {
 		msg := err.Error()
 		if strings.HasPrefix(msg, "flag provided but not defined: ") {
@@ -98,6 +107,19 @@ Performs a PUT request. A PUT to an account or container will create them. A PUT
 		fmt.Fprintln(os.Stderr, msg)
 	}
 	os.Exit(1)
+}
+
+func fatalf(frmt string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, frmt, args...)
+	fmt.Fprintln(os.Stderr)
+	os.Exit(1)
+}
+
+func verbosef(frmt string, args ...interface{}) {
+	if *globalFlagVerbose {
+		fmt.Fprintf(os.Stderr, frmt, args...)
+		fmt.Fprintln(os.Stderr)
+	}
 }
 
 func helpFlags(flags *flag.FlagSet) {
@@ -120,27 +142,27 @@ func helpFlags(flags *flag.FlagSet) {
 	})
 	opts := brimtext.NewDefaultAlignOptions()
 	opts.Widths = []int{0, brimtext.GetTTYWidth() - firstWidth - 2}
-	fmt.Fprint(os.Stderr, brimtext.Align(data, opts))
+	fmt.Print(brimtext.Align(data, opts))
 }
 
 func main() {
 	if err := globalFlags.Parse(os.Args[1:]); err != nil || len(globalFlags.Args()) == 0 {
-		help(globalFlags, err)
+		fatal(err)
 	}
 	if *globalFlagAuthURL == "" {
-		help(globalFlags, errors.New("No Auth URL set; use -A"))
+		fatalf("No Auth URL set; use -A")
 	}
 	if *globalFlagAuthUser == "" {
-		help(globalFlags, errors.New("No Auth User set; use -U"))
+		fatalf("No Auth User set; use -U")
 	}
 	if *globalFlagAuthKey == "" && *globalFlagAuthPassword == "" {
-		help(globalFlags, errors.New("No Auth Key or Password set; use -K or -P"))
+		fatalf("No Auth Key or Password set; use -K or -P")
 	}
 	c, resp := client.NewClient(*globalFlagAuthTenant, *globalFlagAuthUser, *globalFlagAuthPassword, *globalFlagAuthKey, *globalFlagStorageRegion, *globalFlagAuthURL, *globalFlagInternalStorage)
 	if resp != nil {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		fatal("Auth responded with %d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("Auth responded with %d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	cmd := ""
 	args := append([]string{}, globalFlags.Args()...)
@@ -159,8 +181,10 @@ func main() {
 		post(c, args)
 	case "put":
 		put(c, args)
+	case "upload":
+		upload(c, args)
 	default:
-		help(globalFlags, fmt.Errorf("Unknown command: %s", cmd))
+		fatalf("Unknown command: %s", cmd)
 	}
 }
 
@@ -177,14 +201,14 @@ func delet(c client.Client, args []string) {
 	if resp.StatusCode/100 != 2 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	resp.Body.Close()
 }
 
 func get(c client.Client, args []string) {
 	if err := getFlags.Parse(args); err != nil {
-		help(getFlags, err)
+		fatal(err)
 	}
 	container, object := parsePath(getFlags.Args())
 	if *getFlagRaw || object != "" {
@@ -199,7 +223,7 @@ func get(c client.Client, args []string) {
 		if resp.StatusCode/100 != 2 {
 			bodyBytes, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close()
-			fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+			fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 		}
 		if *getFlagRaw || object == "" {
 			data := [][]string{}
@@ -218,7 +242,7 @@ func get(c client.Client, args []string) {
 			fmt.Print(brimtext.Align(data, opts))
 		}
 		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-			fatal("%s", err)
+			fatal(err)
 		}
 		return
 	}
@@ -227,7 +251,7 @@ func get(c client.Client, args []string) {
 		if resp.StatusCode/100 != 2 {
 			bodyBytes, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close()
-			fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+			fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 		}
 		if *getFlagNameOnly {
 			for _, entry := range entries {
@@ -255,7 +279,7 @@ func get(c client.Client, args []string) {
 	if resp.StatusCode/100 != 2 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	if *getFlagNameOnly {
 		for _, entry := range entries {
@@ -274,7 +298,7 @@ func get(c client.Client, args []string) {
 
 func head(c client.Client, args []string) {
 	if err := headFlags.Parse(args); err != nil {
-		help(headFlags, err)
+		fatal(err)
 	}
 	container, object := parsePath(headFlags.Args())
 	var kstrip string
@@ -299,7 +323,7 @@ func head(c client.Client, args []string) {
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if resp.StatusCode/100 != 2 {
-		fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	data := [][]string{}
 	ks := []string{}
@@ -350,7 +374,7 @@ func put(c client.Client, args []string) {
 	if resp.StatusCode/100 != 2 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	resp.Body.Close()
 }
@@ -368,15 +392,94 @@ func post(c client.Client, args []string) {
 	if resp.StatusCode/100 != 2 {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		fatal("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+		fatalf("%d %s - %s", resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
 	}
 	resp.Body.Close()
 }
 
-func fatal(frmt string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, frmt, args...)
-	fmt.Fprintln(os.Stderr)
-	os.Exit(1)
+func upload(c client.Client, args []string) {
+	if len(args) == 0 {
+		fatalf("<sourcepath> is required for upload")
+	}
+	sourcepath := args[0]
+	container, object := parsePath(args[1:])
+	if container == "" {
+		abscwd, err := filepath.Abs(".")
+		if err != nil {
+			fatal(err)
+		}
+		container = filepath.Base(abscwd)
+	}
+	verbosef("Ensuring container %q exists.", container)
+	resp := c.PutContainer(container, globalFlagHeaders.Headers())
+	if resp.StatusCode/100 != 2 {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		fatalf("PUT %s %d %s - %s", container, resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+	}
+	resp.Body.Close()
+	concurrency := *globalFlagConcurrency
+	if concurrency < 1 {
+		concurrency = 1
+	}
+	uploadChan := make(chan string, concurrency-1)
+	wg := sync.WaitGroup{}
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				path := <-uploadChan
+				if path == "" {
+					break
+				}
+				verbosef("Uploading %q to %q %q.", path, container, object+path)
+				f, err := os.Open(path)
+				if err != nil {
+					if *globalFlagContinueOnError {
+						fmt.Fprintf(os.Stderr, "ERROR Uploading %q to %q %q: %s\n", path, container, object+path, err)
+						continue
+					} else {
+						fatal(err)
+					}
+				}
+				defer f.Close()
+				resp := c.PutObject(container, object+path, globalFlagHeaders.Headers(), f)
+				defer resp.Body.Close()
+				if resp.StatusCode/100 != 2 {
+					bodyBytes, _ := ioutil.ReadAll(resp.Body)
+					if *globalFlagContinueOnError {
+						fmt.Fprintf(os.Stderr, "ERROR with PUT %s/%s %d %s - %s\n", container, object+path, resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+						continue
+					} else {
+						fatalf("PUT %s/%s %d %s - %s", container, object+path, resp.StatusCode, http.StatusText(resp.StatusCode), string(bodyBytes))
+					}
+				}
+			}
+		}()
+	}
+	fi, err := os.Stat(sourcepath)
+	if err != nil {
+		fatal(err)
+	}
+	// This "if" is so a single file upload that happens to be a symlink will work.
+	if fi.Mode().IsRegular() {
+		uploadChan <- sourcepath
+	} else {
+		// This "if" is to handle when the user-given path is a symlink to a directory; we normally want to skip symlinks, but not in this initial case.
+		if !strings.HasSuffix(sourcepath, string(os.PathSeparator)) {
+			sourcepath += string(os.PathSeparator)
+		}
+		filepath.Walk(sourcepath, func(path string, info os.FileInfo, err error) error {
+			if err != nil || !info.Mode().IsRegular() {
+				return nil
+			}
+			uploadChan <- path
+			return nil
+		})
+	}
+	close(uploadChan)
+	wg.Wait()
 }
 
 func parsePath(args []string) (string, string) {
