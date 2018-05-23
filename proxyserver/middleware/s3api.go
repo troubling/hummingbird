8a2d3e2f2577b7c124e9c132a2075c675d874789
Chuck Thier <cthier@gmail.com>
2018-05-23 18:33:33 +0000

    HUM-756 Implement S3 Container DELETE api

--- proxyserver/middleware/s3api.go
+++ proxyserver/middleware/s3api.go
@@ -166,6 +166,25 @@ func (s *s3ApiHandler) handleObjectRequest(writer http.ResponseWriter, request *
 }
 
 func (s *s3ApiHandler) handleContainerRequest(writer http.ResponseWriter, request *http.Request) {
+	ctx := GetProxyContext(request)
+
+	if request.Method == "DELETE" {
+		newReq, err := ctx.newSubrequest("DELETE", s.path, http.NoBody, request, "s3api")
+		if err != nil {
+			srv.SimpleErrorResponse(writer, http.StatusInternalServerError, err.Error())
+		}
+		newReq.Header.Set("Accept", "application/json")
+		cap := NewCaptureWriter()
+		ctx.serveHTTPSubrequest(cap, newReq)
+		if cap.status/100 != 2 {
+			srv.StandardResponse(writer, cap.status)
+			return
+		} else {
+			writer.WriteHeader(204)
+			return
+		}
+	}
+
 	// If we didn't get to anything, then return no implemented
 	srv.SimpleErrorResponse(writer, http.StatusNotImplemented, "Not Implemented")
 }
